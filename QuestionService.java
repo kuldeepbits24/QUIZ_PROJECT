92a9e79cd72da52f2ee4a9fbdcadf95ee80ed810
Kuldeep Chaudhary <kuldeepbits24@gmail.com>
2025-12-02 15:36:29 +0530

    Question SERVICE UPDATED

---
+++ QuestionService.java
@@ -0,0 +1,9 @@
+public class QuestionService {
+
+
+    Questions[] questions = new Questions[5];  // we are creating an array of size 5; 
+
+    public void display() {
+        System.out.println("Displaying Questions...");
+    }
+}
--- quiz.java
+++
@@ -1,5 +0,0 @@
-public class quiz {
-    public static void main(String[] args) {
-        System.out.print("WELCOME TO THE QUIZ."); 
-    }
-}
